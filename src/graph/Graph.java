b1ed46c9f0bf94771ec6bf394d1c563e5c2cdc7a
D_MENT <44676701+rocket111185@users.noreply.github.com>
2020-02-12 11:36:39 +0200

    Added class Node

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -3,7 +3,27 @@ package graph;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+class Node extends StackPane {
+    Node (Object label, int x, int y, int radius) {
+        // Create a circle and set color properties
+        Circle circle = new Circle(radius);
+        circle.setFill(Color.WHITE);
+        circle.setStroke(Color.BLACK);
+        // Text is a number of node
+        Text text = new Text(label.toString());
+        text.setFont(Font.font(18));
+        // Create a group of circle and text and set coordinates
+        this.getChildren().addAll(circle, text);
+        this.setLayoutX(x);
+        this.setLayoutY(y);
+    }
+}
 
 public class Graph extends Application {
     
